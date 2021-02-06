76d73d53a0c8c4f574f0d3cde718fcd8647cb6e2
PurePureMilk <gtkpham@gmail.com>
2021-02-06 10:29:48 -0500

    part 2 done

---
+++ Quiz Game/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+	public Question[] questions; //array of question that fill in the inspector
+	private static List<Question> unansweredQuestions; //persists throught scenes
+	
+	private Question currentQuestion;
+	
+	void Start(){
+		if (unansweredQuestions == null || unansweredQuestions.Count == 0) {
+			unansweredQuestions = questions.ToList<Question>();
+		}
+		
+		GetRandomQuestion();
+		Debug.Log(currentQuestion.fact + " is " + currentQuestion.isTrue);
+	}
+	
+	void GetRandomQuestion () {
+		int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
+		currentQuestion = unansweredQuestions[randomQuestionIndex];
+
+		unansweredQuestions.RemoveAt(randomQuestionIndex);
+	}
+}
